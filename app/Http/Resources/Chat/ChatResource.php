6735b3ce7e352f71f0a715d5ea11f5862c624880
Mahbubul Hasan <mahbubul161@gmail.com>
2023-11-11 14:43:53 +0600

    Fetch chat histories

--- app/Http/Resources/Chat/ChatResource.php
+++ app/Http/Resources/Chat/ChatResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Chat;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource {
+class ChatResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
